1ab24099a8ac790e51eb49177f3bf189006601ce
James Scalise <jamescscalise@gmail.com>
2019-09-07 04:40:31 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,13 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species
+  @@a
+  
+  def initialize(name)
+    @name = name
+    @species = "human"
+  end
+  
+  def say_species
+    return "I am a human."
+  end
 end
