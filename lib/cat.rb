461254492bdc3d59e3f51afae5af7e608ab118e1
James Scalise <jamescscalise@gmail.com>
2019-09-07 04:53:38 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,9 +1,11 @@
 class Cat
-  attr_reader :name, :owner, :mood
+  attr_reader :name, :mood
+  attr_accessor :owner
   
   def initialize(name, owner)
     @name = name
     @owner = owner
     mood = "nervous"
   end
+  
 end
